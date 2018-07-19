c409161e3c481e322edc9c2c4d9dc73e9b683f8c
fancyaction <labcoatdisaster@gmail.com>
2018-07-19 18:22:44 -0500

    Add Dashboard welcome

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { PostList, PostCreate, PostEdit } from './Posts';
 import { UserList } from './Users';
+import Dashboard from './Dashboard';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
---
+++ src/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+  <Card>
+    <CardHeader title="Welcome to the administration" />
+    <CardContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas
+      alias quod? Quaerat explicabo fuga, dignissimos sequi, earum sed
+      voluptatibus delectus repellendus tempora quidem officiis hic consequatur
+      quod vitae voluptate.
+    </CardContent>
+  </Card>
+);
