68a57c55c6e9ddac6c66f84565ba880e03c4dca6
fancyaction <labcoatdisaster@gmail.com>
2018-07-19 18:41:59 -0500

    Add user authentication

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import jsonServerProvider from 'ra-data-json-server';
 import { PostList, PostCreate, PostEdit } from './Posts';
 import { UserList } from './Users';
 import Dashboard from './Dashboard';
+import AuthProvider from './AuthProvider';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    dataProvider={dataProvider}
+    authProvider={AuthProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
