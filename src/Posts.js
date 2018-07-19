1c179d50d57b7d6eac064da526f07efe4a56f4d7
fancyaction <labcoatdisaster@gmail.com>
2018-07-19 17:51:42 -0500

    Add PostCreate

--- src/Posts.js
+++ src/Posts.js
@@ -45,3 +45,16 @@ export const PostEdit = props => (
     </SimpleForm>
   </Edit>
 );
+
+export const PostCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <ReferenceInput label="User" source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <LongTextInput source="body" />
+    </SimpleForm>
+  </Create>
+);
