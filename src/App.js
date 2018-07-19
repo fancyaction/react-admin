e17b5b72560c3ae32ded1c7f05d45512b8c1f0a0
fancyaction <labcoatdisaster@gmail.com>
2018-07-19 18:04:51 -0500

    Add post edit and create to admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from './Posts';
+import { PostList, PostCreate, PostEdit } from './Posts';
 import { UserList } from './Users';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
     <Resource name="users" list={UserList} />
   </Admin>
 );
